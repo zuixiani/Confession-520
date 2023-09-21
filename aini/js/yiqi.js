17edffebd30007628fcb9cba1ef4595aa838e60a
zuixiani <zuixian666@gmail.com>
2023-09-21 17:09:55 +0800

    首次提交

---
+++ aini/js/yiqi.js
@@ -0,0 +1,45 @@
+/*醉仙美化 QQ874824173*/
+// JavaScript Document
+var Dianji=0;
+window.onload=function(){
+	var buhao = document.getElementById("buhao");
+	var hao = document.getElementById("hao");
+	buhao.onclick=function(){
+		Dianji++;
+		if(Dianji==1){
+	   			alert("小姐姐在考虑一下呗");
+		   		alert("你是我见过的最温柔可爱善良体贴的的女孩");
+		   		alert("一生一世只爱你一人");
+		   		alert("遇见你不容易，错过你会很可惜");
+		   		alert("你眼里有春和秋，胜过我见过的一切山川河流");
+		   		alert("有你的将来才叫余生，其他的只是以后");
+		   	alert("你是我一生遇见一次的惊喜");
+		   	alert("我步步都回头，山都是故人眸，柳是纤纤手，遇你之后步步都难走");
+		   	alert("指尖的幸福，只有你懂；手心的温柔，只有你懂");
+		   	alert("我的喜欢写在风里，从此整个世界都是你");
+		   	alert("我愿执笔弃花间，从此以后，离经易道，只为你");
+		   	alert("我愿红线永相系，自此经年，江湖之远，只因你");
+		   alert("纵有桃花朵朵开，取一朵放心上足矣");
+		   	alert("终于为了你，覆了那一雨江南，断了那红尘潇潇");
+		   alert("良辰美景，可愿与君共赴黄泉，若你眉眼为我停留半分，又怎会看不见我深情如初 ");
+		   alert("煎雪落雨，心上是你，欢喜躲在眉目里");
+		   alert("愿琴瑟在御，岁月静好；愿执子之手，与子偕老");
+		   alert("许我浮生一世安 ，还你笑颜承你欢");
+		  	alert("人生有情泪沾衣,江水江花岂终极");
+		   alert("我骄傲，在我的一生中遇见过你");
+		   alert("我庆幸，在我的一生中没错过你"); 
+		   alert("灯塔永远不会孤单，只因你是海岸");
+		  	alert("小船永远不在漂泊，只因你是港湾");
+		  	alert("不论世事怎样变迁，你都是我的唯一");
+	   	alert("不论未来如何迷茫，你都是我的永远");
+		  	alert("家务我做");
+		  	alert("房子写你的名字");
+		  	alert("工资全上交，零花钱你分配");
+		   alert("保大");
+		  	alert("还是不喜欢我嘛？");
+		  	alert("好吧，再来一遍吧，最后一遍咯。");
+		   	window.location.href="zuihou.html";
+		  		Dianji=1;
+	  }
+         }
+}
